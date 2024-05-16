0f6ce8c7caa231bf297fde0ce2f86ada078cab50
gsavelev <super.gleb@pm.me>
2024-05-16 16:09:31 +0300

    move init of zsh at the end

--- bin/build-env.sh
+++ bin/build-env.sh
@@ -6,10 +6,11 @@ apt-get install zsh -y
 chsh -s $(which zsh)
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 cp -p $HOME/dotfiles/.zshrc $HOME/.zshrc
-zsh
 
 #vim
 apt-get install zsh -y
 mkdir $HOME/.vim/
 cp -rp $HOME/dotfiles/.vim/* $HOME/.vim/
 cp -rp $HOME/dotfiles/.vimrc $HOME/.vimrc
+
+zsh
